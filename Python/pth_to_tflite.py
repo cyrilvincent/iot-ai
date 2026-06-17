8687f6182ed0fcf38179c869c1c0032bb98b3c24
Cyril Vincent <contact@cyrilvincent.com>
2026-06-17 16:49:08 +0200

    vitis

--- Python/pth_to_tflite.py
+++ Python/pth_to_tflite.py
@@ -8,8 +8,8 @@ model.load_state_dict(torch.load(name))
 
 input_tensor = torch.rand(1, 30)
 
-edge_model = litert_torch.convert(model.eval(), input_tensor)
-edge_model.export(model + ".tflite")
+edge_model = litert_torch.convert(model.eval(), (input_tensor,))
+edge_model.export(name + ".tflite")
 
 # Sur WSL2 Ubuntu
 # cd /mnt/c/Users/conta/git-CVC/Formation/IoT/git-iot-ai/python
