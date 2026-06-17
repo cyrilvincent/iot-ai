95dc5cca426d982cc98be02ae612f058c08194ab
Cyril Vincent <contact@cyrilvincent.com>
2026-06-17 22:40:36 +0200

    rpi

--- Python/tflite_rpi.py
+++ Python/tflite_rpi.py
@@ -1,7 +1,7 @@
 import numpy as np
 from tflite_runtime.interpreter import Interpreter
 
-interpreter = Interpreter(model_path="data/mnist/mnist_cnn.h5")
+interpreter = Interpreter(model_path="data/mnist/mnist_cnn_fp32.tflite")
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
