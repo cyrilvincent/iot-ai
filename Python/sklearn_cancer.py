0d05c71d7776a44060a6eb8118302d6b71d864e4
cyrilvincent <contact@cyrilvincent.com>
2026-07-08 14:28:22 +0200

    atp

--- Python/sklearn_cancer.py
+++ Python/sklearn_cancer.py
@@ -10,3 +10,11 @@ import sklearn.model_selection as ms
 df = pd.read_csv("data/breast-cancer/data.csv")
 y = df["diagnosis"]
 x = df.drop(["diagnosis", "id"], axis=1)
+
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2, random_state=42)
+
+model = n.KNeighborsClassifier(n_neighbors=3)
+model.fit(xtrain, ytrain)
+
+print(model.score(xtrain, ytrain))
+print(model.score(xtest, ytest))
