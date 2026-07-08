5f015cb3be36e40fb659d09a089f27d1c9cdc1d3
cyrilvincent <contact@cyrilvincent.com>
2026-07-08 11:55:17 +0200

    atp

---
+++ Arduino/platformio/house/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+float f(float x) {
+    return 41.635 * x -311;
+}
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+    float result = f(100);
+    Serial.println(result);
+    delay(1000);
+}
