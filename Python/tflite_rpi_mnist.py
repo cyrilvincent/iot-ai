d603d74629aff7cab515b44f8223109acdc1689b
Cyril Vincent <contact@cyrilvincent.com>
2026-07-22 14:05:54 +0200

    atp

---
+++ Python/tflite_rpi_mnist.py
@@ -0,0 +1,21 @@
+import numpy as np
+from tflite_runtime.interpreter import Interpreter
+
+interpreter = Interpreter(model_path="data/mnist/mnist_cnn_fp32.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+with np.load("data/mnist/mnist.npz", allow_pickle=True) as f:
+    x_test = f['x_test']
+
+x_test = x_test / 255.
+input_data = x_test[0].astype(np.float32).reshape(1,28,28,1)
+
+interpreter.set_tensor(input_details[0]['index'], input_data)
+interpreter.invoke()
+
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
+print(np.argmax(output_data))
